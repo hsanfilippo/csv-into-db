694babe2662d3ebab3bb27fdf0a28d4da91086f9
hsanfilippo <henrique.sanfilippo@gmail.com>
2024-07-14 01:40:09 +0900

    create_psql fix

--- csv_into_db/create_psql.py
+++ csv_into_db/create_psql.py
@@ -1,13 +1,13 @@
 # CREATES A BRAND NEW DB BASED ON A CSV FILE:
 def create_psql(tableName: str, colNames: list, colTypes: list) -> str: 
-    import conv_types_psql
+    import conv_types_psql as conv
 
     query_parts = list()
     query_content = str()
     final_query = str()
 
     try:
-        for col_name, col_type in zip(colNames, conv_types_psql.conv_types_psql(colTypes)):
+        for col_name, col_type in zip(colNames, conv.conv_types_psql(colTypes)):
             query_parts.append(f"{col_name} {col_type}")
             query_content = ", ".join(query_parts)
             final_query = f"""CREATE TABLE {tableName} ({query_content});"""
@@ -16,6 +16,3 @@ def create_psql(tableName: str, colNames: list, colTypes: list) -> str:
         print(f"{exc}")
     
     return final_query
-
-colunas_nome = ['nome', 'telefone', 'cidade']
-colunas_tipo = ['str', 'str', 'str']
